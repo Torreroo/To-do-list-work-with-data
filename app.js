6ef5da61bbf43845131624256583ff48f9ce2501
Алексей Козачок <aleksejkozacok@MacBook-Air-Aleksej.local>
2026-05-28 22:21:38 +0300

    finsish edit logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const input = document.getElementById("item-input");
-const btn = document.getElementById("add-btn");
+const addBtn = document.getElementById("add-btn");
 const list = document.getElementById("list");
 const listItems = document.querySelectorAll("li")
-
+let editingItem = null;
 //delete functionality 
 for (let listItem of listItems) {
 
@@ -17,35 +17,55 @@ for (let listItem of listItems) {
 
 
 //create functionality 
-btn.addEventListener("click", () => {
-let inputValue = input.value //bread
-    if (inputValue.length > 0) {
-        //delete btn logic
-        let deleteButton = document.createElement("button");
-        deleteButton.textContent = 'X'
+addBtn.addEventListener("click", () => {
+    let inputValue = input.value //bread
 
-        //edit btn logic 
+    //delete btn logic
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = 'X'
+
+    //edit btn logic 
     let editBtn = document.createElement("button");
     editBtn.textContent = "Edit";
 
+    //list item element
+    let newItem = document.createElement("li");
 
-        let newItem = document.createElement("li");
+    if (inputValue.length > 0) {
+
+
         newItem.textContent = inputValue;
         newItem.append(deleteButton)
         newItem.append(editBtn);
+        //logic to edit
         editBtn.addEventListener("click", () => {
             input.value = inputValue;
+            addBtn.textContent = 'Save'
+            editingItem = newItem;
         });
+
         //logic to delete item
         deleteButton.addEventListener("click", () => {
-        newItem.remove();
+            newItem.remove();
         })
- 
-        list.append(newItem)
-        input.value = ''
-        
+
+
+
     } else {
         alert("Please, type the text");
     }
 
+
+    if (addBtn.textContent == 'Add') {
+        list.append(newItem)
+        input.value = ''
+
+    } else {
+  //click on save
+        editingItem.firstChild.textContent = inputValue;
+    
+        addBtn.textContent = 'Add'
+        input.value = ''
+        editingItem = null;
+    }
 });
